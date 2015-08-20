fcdab699751a66fde2d2f7fddc771e01ef83263e
Didares <joseantoniojimenezmorales@gmail.com>
2015-08-21 01:54:55 +0200

    Codificador x^2
    Codificador que asigna un valor a cada letra, lo opera y devuelve el
    resultado, con posibilidad de crear un .txt con el.

---
+++ Codificador x^2.cpp
@@ -0,0 +1,100 @@
+/* 
+
+   Programa que cifra/descifra palabras en función 
+   de su valor y de la funcion y=x^2
+
+   Estructura:
+              1. Asociación de letra a su número
+              2. Interfaz
+              3. Codificación
+              4. Resultado de la codificación en pantalla
+              5. Opción de crear un .txt con el mensaje codificado//Todavia en desarrollo
+           
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+      
+int main();
+
+
+# 1. Asociación de cada letra con un numero y variables
+
+int a=1;
+int b=2;
+int c=3;
+int d=4;
+int e=5;
+int f=6;
+int g=7;
+int h=8;
+int i=9;
+int j=10;
+int k=11;
+int l=12;
+int m=13;
+int n=14;
+int o=15;
+int p=16;
+int q=17;
+int r=18;
+int s=29;
+int t=20;
+int u=22;
+int v=23;
+int w=23;
+int x=24;
+int y=25;
+int z=26;
+
+int nLetras;
+
+char texto[];
+char codigo[];
+
+int archivo;
+
+# 2. Interfaz
+
+cout << "Introduzca el texto a codificar"endl;
+cin >> texto;
+
+
+#Codificación
+
+nLetras = sizeof(texto);
+
+do{
+        codigo = (texto[-1++1])^2;
+        cout << codigo << " ";     
+           
+}while(texto=>0);
+
+#Opción de sacar el codigo resultante en un .txt
+
+cout << "¿Desea transferir el codigo resultante a un archivo .txt?: " << endl;
+cout << "1. Sí" << endl;
+cout << "2. No" << endl;
+
+cin >> archivo;
+
+if(archivo=1){
+              cout << "Nombre del archivo: ";
+              cin >> archivo;
+              ofstream fs (archivo);
+              fs << codigo << endl;
+              fs.close();
+              
+}else{
+      return 0;
+}
+
+}
+
+
+
+
+
